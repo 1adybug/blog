7a396c9da185ea56ca6ed1d88a6299e3d32fb810
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-08-21 17:55:35 +0800

    ✨feature: 新增 pwa 支持

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,7 +104,33 @@ const config: Config = {
             theme: prismThemes.github,
             darkTheme: prismThemes.dracula
         }
-    } satisfies Preset.ThemeConfig
+    } satisfies Preset.ThemeConfig,
+    plugins: [
+        [
+            "@docusaurus/plugin-pwa",
+            {
+                debug: true,
+                offlineModeActivationStrategies: ["appInstalled", "standalone", "queryString"],
+                pwaHead: [
+                    {
+                        tagName: "link",
+                        rel: "icon",
+                        href: "/img/docusaurus.png"
+                    },
+                    {
+                        tagName: "link",
+                        rel: "manifest",
+                        href: "/manifest.json" // your PWA manifest
+                    },
+                    {
+                        tagName: "meta",
+                        name: "theme-color",
+                        content: "rgb(37, 194, 160)"
+                    }
+                ]
+            }
+        ]
+    ]
 }
 
 export default config
