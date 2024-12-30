785b580fc276a25e5f86a5b151c8e1e58e6cb723
陆子虚 <lurongv@qq.com>
2024-12-30 10:20:05 +0800

    ✨feature: 添加 prettier 配置文件

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 
-import { readFileSync } from "fs"
 import { parse } from "path"
 import { globSync } from "glob"
 
@@ -27,25 +26,11 @@ const assetExtsRegStr = `\\.(${assetExts.join("|")}|${assetExts.join("|").toUppe
 
 const assetQueryRegStr = "(\\?[a-zA-Z0-9]+)?"
 
-const namespaces = unique(
-    unique(
-        globSync("**/package.json", { withFileTypes: true, cwd: import.meta.dirname })
-            .filter(path => path.isFile())
-            .map(path => path.fullpath()),
-    )
-        .map(path => JSON.parse(readFileSync(path, "utf8")))
-        .map(json =>
-            Object.keys({
-                ...json.dependencies,
-                ...json.devDependencies,
-                ...json.peerDependencies,
-                ...json.optionalDependencies,
-            }),
-        )
-        .flat()
-        .filter(dep => dep.startsWith("@"))
-        .map(dep => dep.split("/")[0].slice(1)),
-)
+const folders = unique(
+    globSync("**/*", { withFileTypes: true, cwd: import.meta.dirname, ignore: ["node_modules/**"] })
+        .filter(path => path.isDirectory())
+        .map(path => path.name),
+).sort()
 
 /**
  * @type {import("prettier").Options}
@@ -55,16 +40,20 @@ const config = {
     tabWidth: 4,
     arrowParens: "avoid",
     printWidth: 160,
-    plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-lint-md"],
+    plugins: ["@ianvs/prettier-plugin-sort-imports"],
     importOrder: [
+        "^react(/.+)?$",
+        "^react-dom(/.+)?$",
+        "^react-native(/.+)?$",
         "<BUILTIN_MODULES>",
-        `^@(${namespaces.join("|")})/`,
+        "^@[^/]",
         "<THIRD_PARTY_MODULES>",
+        ...folders.map(folder => ["", `^@/?${folder}.+?(?<!${assetExtsRegStr}${assetQueryRegStr})$`]).flat(),
         "",
-        `^@.+?(?<!${assetExtsRegStr}${assetQueryRegStr})$`,
+        `^@/.+?(?<!${assetExtsRegStr}${assetQueryRegStr})$`,
         `^\\.{1,2}/.+?(?<!${assetExtsRegStr}${assetQueryRegStr})$`,
         "",
-        `^@.+?${assetExtsRegStr}${assetQueryRegStr}$`,
+        `^@/.+?${assetExtsRegStr}${assetQueryRegStr}$`,
         `^\\.{1,2}/.+?${assetExtsRegStr}${assetQueryRegStr}$`,
     ],
     importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
