95864dc7d663a62923c1e506b72407e233119357
陆子虚 <lurongv@qq.com>
2025-01-09 17:06:31 +0800

    ✨feature: 添加代码片段同步脚本，并更新 package.json

---
+++ scripts/syncSnippets.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+
+import { copyFile } from "fs/promises"
+import { homedir } from "os"
+import { join } from "path"
+import { execAsync } from "soda-nodejs"
+
+/**
+ * @param {string | undefined} [cwd]
+ */
+export async function hasChangeNoCommit(cwd) {
+    const status = await execAsync("git status", { cwd })
+    return !status.includes("nothing to commit, working tree clean")
+}
+
+async function main() {
+    if (await hasChangeNoCommit()) {
+        console.error("请先提交当前修改")
+        return
+    }
+    const userDir = homedir()
+    const path = join(userDir, "AppData/Roaming/Code/User/snippets/global.code-snippets")
+    await copyFile(path, "static/global.code-snippets")
+    if (await hasChangeNoCommit(".")) {
+        await execAsync("git add static/global.code-snippets")
+        await execAsync("git commit -m '✨feature: 更新代码片段'")
+        await execAsync("git push")
+    }
+}
+
+main()
