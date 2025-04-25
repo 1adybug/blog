ed03d9b6bce2227e468975f9185589d4060904a1
陆子虚 <lurongv@qq.com>
2025-04-25 09:35:08 +0800

    ✨feature: 同步更新 Cursor 设置

--- scripts/syncCursor.mjs
+++ scripts/syncCursor.mjs
@@ -21,14 +21,16 @@ async function main() {
     const userDir = homedir()
     const path = join(userDir, "AppData/Roaming/Cursor/User/snippets/global.code-snippets")
     await copyFile(path, "static/global.code-snippets")
+    const path2 = join(userDir, "AppData/Roaming/Cursor/User/settings.json")
+    await copyFile(path2, "static/settings.json")
     if (!(await hasChangeNoCommit("."))) {
-        console.error("没有发现代码片段的变化")
+        console.error("没有发现 Cursor 设置的变化")
         return
     }
     await execAsync("git add .")
-    await execAsync(`git commit -m "✨feature: 更新代码片段"`)
+    await execAsync(`git commit -m "✨feature: 更新 Cursor 设置"`)
     await execAsync("git push")
-    console.log("代码片段同步成功")
+    console.log("Cursor 设置同步成功")
 }
 
 main()
